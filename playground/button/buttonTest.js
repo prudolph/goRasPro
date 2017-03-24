d9e1b10e1d172b2445c2e4754fbc72da5aacb672
prudolph <paul.e.rudolph@gmail.com>
2017-03-24 21:25:37 +0000

    updated some of the scripts

--- playground/button/buttonTest.js
+++ playground/button/buttonTest.js
@@ -1,6 +1,7 @@
 // button is attached to pin 17, led to 18
 var GPIO = require('onoff').Gpio,
-    button = new GPIO(17, 'in', 'both');
+    led = new GPIO(22, 'out'),
+    button = new GPIO(27, 'in', 'both');
 
 // define the callback function
 function state(err, state) {
@@ -9,14 +10,20 @@ function state(err, state) {
   // 1 == pressed, 0 == not pressed
   if(state == 1) {
     // turn LED on
+
+    led.writeSync(1);
+
+
     console.log("Button Pressed");
   } else {
     // turn LED off
     console.log("Button Released");
+        led.writeSync(0);
   }
 
 }
 
+console.log("Watching button");
 // pass the callback function to the
 // as the first argument to watch()
 button.watch(state);
